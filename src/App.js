52d2cdbd0a07de2200d3373fc140fe21233632cb
Your Name <you@example.com>
2020-12-11 12:31:29 -0500

    fixed error on procedure details page

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { BrowserRouter, Route} from 'react-router-dom'
 
 import Procedures from './components/Procedures';
+import ProcedureDetails from './components/ProcedureDetails';
 import Home from './components/Home';
 import Contact from './components/Contact';
 import Nav from './components/Nav';
@@ -8,7 +9,25 @@ import Nav from './components/Nav';
 import './App.css';
 
 function App() {
-  const proceduresList = ['root canal', 'crown', 'cleaning']
+  const proceduresList = [
+    {
+      name: 'Root Canal',
+      id: 1,
+      description: 'Very painful'
+    },
+    {
+      name: 'Crown',
+      id: 2,
+      description: 'Absolutely awful'
+    },
+    {
+      name: 'Cleaning',
+      id: 3,
+      description: 'Routine torture'
+    },
+  ]
+
+
   return (
     <BrowserRouter>
       <div className="App">
@@ -18,8 +37,17 @@ function App() {
         <Route path="/" exact component={Home} />
         <Route path="/contact" component={Contact} />
         <Route 
+        exact
         path="/procedures" 
         render={() => {return <Procedures proceduresList={proceduresList}/>}} />
+        <Route 
+        path="/procedures/:procedureId" 
+        render={(routeProps) => { 
+          const id = routeProps.match.params.procedureId
+          const targetProcedure = proceduresList.find((p) => p.id === parseInt(id))
+          return <ProcedureDetails procedure={targetProcedure} />
+        }}
+        />
       </div>
     </BrowserRouter>
   );
--- src/components/Procedures.js
+++ src/components/Procedures.js
@@ -1,12 +1,13 @@
-
+import { Link } from 'react-router-dom'
 function Procedures(props) {
-    const proceduresItems = props.proceduresList.map((p) => {
-        return <li>{p}</li>
-    })
+    console.log(props)
+    const procedureItems = props.proceduresList.map((p) => {
+        return <li><Link to={`/procedures/${p.id}`}>{p.name}</Link></li>
+      })
     return(
         <>
             <h1>Procedures</h1>
-            <ul>{proceduresItems}</ul>
+            <ul>{procedureItems}</ul>
         </>
     )
 }
